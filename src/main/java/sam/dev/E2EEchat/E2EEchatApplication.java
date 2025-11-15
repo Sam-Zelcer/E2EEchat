29ef333903857cd5591656d2fb4b6350e2f34aa1
Sam-Zelcer <samzelcerr@gmail.com>
2025-11-15 23:38:12 +0100

    initial commit

---
+++ src/main/java/sam/dev/E2EEchat/E2EEchatApplication.java
@@ -0,0 +1,13 @@
+package sam.dev.E2EEchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2EEchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2EEchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sam/dev/E2EEchat/E2EEchatApplicationTests.java
@@ -0,0 +1,13 @@
+package sam.dev.E2EEchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2EEchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
